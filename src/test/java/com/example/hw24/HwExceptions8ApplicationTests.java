a449ee60670b9f8dd8777ba822a828305efd2d05
IrinaMa72 <morevaia72@gmail.com>
2023-06-25 22:07:28 +0500

    Initial commit

---
+++ src/test/java/com/example/hw24/HwExceptions8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwExceptions8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
